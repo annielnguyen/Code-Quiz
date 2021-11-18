8c6aed15fd32fe05fc9061ea6d486d7b731acee9
Annie Nguyen <anguyen.aln@gmail.com>
2021-11-17 21:52:29 -0800

    removed instructions & flex styling and added a question box

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var timerElement = document.querySelector(".timer-count");
 var startButton =  document.querySelector(".start_button")
 var timerElement = document.querySelector(".timer-count");
-console.log(startButton)
+var quiz_block   = document.querySelector(".Quiz");
 
 var timer;
 var timerCount;
@@ -9,40 +9,42 @@ var timerCount;
 //Array of questions in quiz
 
 //var questions = ["Inside which HTML element do we put the JavaScript?","Where is the correct place to insert a JavaScript?","What is the correct syntax for referring to an external script called 'xxx.js'?"];
-const myQuestions = [
-    {
-      question: "Inside which HTML element do we put the JavaScript?",
-      answers: {
-        a: '<script>',     
-        b: '<scripting>',
-        c: '<js>',
-        d: '<javascript>',
-      },
-      correctAnswer: "a"
-    },
-    {
-      question: "Where is the correct place to insert a JavaScript?",
-      answers: {
-        a: "Both the <head> section and the <body> section are correct", 
-        b: "The <head> section",
-        c: "The <body> section",
-      },
-      correctAnswer: "a"
-    },
-    {
-      question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-      answers: {
-        a: '<script name="xxx.js"',
-        b: '<script src="xxx.js">', 
-        c: '<script href="xxx.js"',
-      correctAnswer: "b"
-    }
-  ];
+// const myQuestions = [
+//     {
+//       question: "Inside which HTML element do we put the JavaScript?",
+//       answers: {
+//         a: '<script>',     
+//         b: '<scripting>',
+//         c: '<js>',
+//         d: '<javascript>',
+//       },
+//       correctAnswer: "a"
+//     },
+//     {
+//       question: "Where is the correct place to insert a JavaScript?",
+//       answers: {
+//         a: "Both the <head> section and the <body> section are correct", 
+//         b: "The <head> section",
+//         c: "The <body> section",
+//       },
+//       correctAnswer: "a"
+//     },
+//     {
+//       question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+//       answers: {
+//         a: '<script name="xxx.js"',
+//         b: '<script src="xxx.js">', 
+//         c: '<script href="xxx.js"',
+//       correctAnswer: "b"
+//     }
+// ];
 // The startGame function is called when the start button is clicked
 function startGame() {
     timerCount = 100;
     // Prevents start button from being clicked when round is in progress
     startButton.disabled = true;
+    quiz_block.style.display = "block"
+    startButton.innerText = "Quit Quiz"
     startTimer()
   }
 
